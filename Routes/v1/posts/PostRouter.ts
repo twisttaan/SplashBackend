1e7071014f6dcab4af3e4312f82db937b391fdcc
Tristan <69066026+twisttaan@users.noreply.github.com>
2022-03-21 22:07:03 +1100

    feat: GET post

--- Routes/v1/posts/PostRouter.ts
+++ Routes/v1/posts/PostRouter.ts
@@ -1,9 +1,43 @@
 import type { FastifyInstance } from "fastify";
+import Joi from "joi";
+interface postReq {
+  postID: string;
+}
+
+interface postQuery {
+  postID: string;
+}
 
 export default async function BaseRouter(fastify: FastifyInstance) {
-  fastify.get("/", async () => {
-    return { statusCode: 200, message: "Hello World!" };
-  });
+  const { prisma } = fastify;
+
+  fastify.get<{ Body: postReq; Querystring: postQuery }>(
+    "/:postID",
+    {
+      schema: {
+        params: Joi.object().keys({
+          postID: Joi.string().required().length(25),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const post = await prisma.post.findFirst({
+        where: {
+          id: request.query.postID,
+        },
+      });
+
+      if (!post) {
+        return reply.code(404).send({
+          message: "Post not found.",
+        });
+      }
+
+      return reply.code(200).send({
+        post,
+      });
+    }
+  );
 }
 
 export const autoPrefix = "/v1/post";
